2593440c1395976e6b1998af59ef037cb3071bf0
ori.calvo@gmail.com <ori.calvo@gmail.com>
2018-02-24 12:35:24 +0200

    console

--- console.cmd
+++
@@ -1,2 +0,0 @@
-CALL prep.cmd
-"%~dp0\bin\conemu\ConEmu64.exe"
---
+++ src/conemu.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const {downloadTo, directoryExists, spawn, unzipTo} = require("./helpers");
+const readline = require('readline');
+
+const appName = "conemu";
+const appExeFileName = "ConEmu64.exe"
+const url = "https://raw.githubusercontent.com/oricalvo/myenv-packages/master/conemu.zip";
+
+const binDir = path.resolve(__dirname, "../bin");
+const appDir = path.resolve(__dirname, `../bin/${appName}`);
+const appExeFilePath = path.resolve(appDir, appExeFileName);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+main();
+
+async function main() {
+    try {
+        if (!await installed()) {
+            if (await confirm()) {
+                await install();
+            }
+			else {
+				return;
+			}
+        }
+
+        run();
+    }
+    catch(err) {
+        console.error(err);
+    }
+    finally {
+        if(rl) {
+            rl.close();
+        }
+    }
+}
+
+async function installed() {
+    return await directoryExists(appDir);
+}
+
+function confirm() {
+    return new Promise((resolve, reject)=> {
+        rl.question(`Install ${appName}? `, (answer) => {
+            answer = answer.toLowerCase();
+            resolve(answer == "yes" || answer == "y");
+        });
+    });
+}
+
+async function install() {
+    const temp = path.resolve(__dirname, "../temp/temp.zip");
+
+    await downloadTo(url, temp);
+
+    unzipTo(temp, binDir);
+}
+
+function run() {
+    console.log(`Running ${appName} from ${appExeFilePath}`);
+
+    const args = process.argv.slice(2);
+    return spawn(appExeFilePath, args, {
+        detached: true,
+    });
+}
